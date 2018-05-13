d821ff938461bba96c618702085f486f23d119c8
magic_su <szh362680581@qq.com>
2018-05-13 21:54:22 +0800

    add grid 样式

--- src/index.js
+++ src/index.js
@@ -3,8 +3,34 @@ import './scss/main.scss'
 const toolkit = require('./js/toolkit')
 const matrix = toolkit.makeMatrix()
 
-console.log(matrix)
+class Grid {
+    constructor(container) {
+        this._$container = container
+    }
 
-const a = Array.from({ length: 9 }, (v, i) => i)
-console.log(a)
-console.log(toolkit.shuffle(a))
+    build() {
+        const matrix = toolkit.makeMatrix()
+
+        const rowGroupClasses = ['row_g_top', 'row_g_middle', 'row_g_bottom']
+        const colGroupClasses = ['col_g_left', 'col_g_center', 'col_g_right']
+
+        const $cells = matrix.map(rowValues => rowValues.map((cellValue, colIndex) => {
+            return $('<span>')
+                .addClass(colGroupClasses[colIndex % 3])
+                .text(cellValue)
+        }))
+        console.log($cells)
+
+        const $divArray = $cells.map(($spanArray, rowIndex) => {
+            return $('<div>')
+                .addClass('row')
+                .addClass(rowGroupClasses[rowIndex % 3])
+                .append($spanArray)
+        })
+
+        this._$container.append($divArray)
+    }
+}
+
+
+new Grid($('#container')).build()
