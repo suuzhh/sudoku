f2d62ea89f26f9db68cca43e016994e4284098ce
magic_su <szh362680581@qq.com>
2018-05-13 23:17:55 +0800

    add 随机矩阵算法

--- src/js/core/generator.js
+++ src/js/core/generator.js
@@ -1,1 +1,59 @@
 // 生成数独解决方案
+import Toolkit from './toolkit'
+
+class Generator {
+    generate() {
+        // 生成矩阵
+        this.matrix = Toolkit.matrix.makeMatrix()
+        // 生成随机矩阵
+        this.orders = Toolkit.matrix.makeMatrix()
+            .map(row => row.map(((v, i) => i)))
+            .map(row => Toolkit.matrix.shuffle(row))
+
+        for (let n = 1; n <= 9; n++) {
+            this.fillNumber(n)
+        }
+    }
+
+    fillNumber(n) {
+        this.fillRow(n, 0)
+    }
+
+    /**
+     * 填充 宫
+     * @param {*} n 需要填充的数字
+     * @param {*} rowIndex 所在的宫
+     */
+    fillRow(n, rowIndex) {
+        if (rowIndex > 8) {
+            return true
+        }
+
+        const row = this.matrix[rowIndex]
+        // 随机位置矩阵 的 当前 row
+        const orders = this.orders[rowIndex]
+
+        for (let i = 0; i < 9; i++) {
+            const colIndex = orders[i]
+            // 如果这个位置已经有值， 跳过
+            if (row[colIndex]) {
+                continue
+            }
+
+            // 检查这个位置是否可以填 n 
+            if (!Toolkit.matrix.checkFillable(this.matrix, n, rowIndex, colIndex)) {
+                continue
+            }
+
+            row[colIndex] = n
+            // 去下一行填写， 如果没填进去 就继续寻找当前行的下一个位置
+            if (!this.fillRow(n, rowIndex + 1)) {
+                row[colIndex] = 0
+                continue
+            }
+
+            return true
+        }
+
+    }
+}
--- src/js/core/toolkit.js
+++ src/js/core/toolkit.js
@@ -28,14 +28,63 @@ const matrixToolkit = {
             console.log(array)
         }
         return array
+    },
+
+    /**
+     * TODO: 检查指定位置可以填写数字
+     */
+    checkFillable(matrix, n, rowIndex, colIndex) {
+        const row = matrix[rowIndex]
+        const column = this.makeRow().map((v, i) => matrix[i][colIndex])
+        const { boxIndex } = boxToolkit.convertToBoxIndex(rowIndex, colIndex)
+        const box = boxToolkit.getBoxCells(matrix, boxIndex)
+        // TODO: 检测宫内数据
+
+
+        return true
     }
+
 }
 
 /**
  * 宫坐标系工具
  */
 const boxToolkit = {
-    //TODO:
+    getBoxCells(matrix, boxIndex) {
+        const startRowIndex = Math.floor(boxIndex / 3) * 3
+        const startColIndex = boxIndex % 3 * 3
+        const result = []
+        for (let cellIndex = 0; cellIndex < 9; cellIndex++) {
+            const rowIndex = startRowIndex + Math.floor(cellIndex / 3)
+            const colIndex = startColIndex + cellIndex % 3
+            result.push(matrix[rowIndex][colIndex])
+        }
+        return result
+    },
+
+    /**
+     * 根据行列坐标获得 宫坐标和宫内坐标
+     * @param {*} rowIndex 行坐标
+     * @param {*} colIndex 列坐标
+     */
+    convertToBoxIndex(rowIndex, colIndex) {
+        return {
+            boxIndex: Math.floor(rowIndex / 3) * 3 + Math.floor(colIndex / 3),
+            cellIndex: rowIndex % 3 * 3 + colIndex % 3
+        }
+    },
+
+    /**
+     * 根据宫坐标和宫内坐标 获得对应的行列坐标
+     * @param {*} boxIndex 宫坐标
+     * @param {*} cellIndex 宫内坐标
+     */
+    convertFromBoxIndex(boxIndex, cellIndex) {
+        return {
+            rowIndex: Math.floor(boxIndex / 3) * 3 + Math.floor(cellIndex / 3),
+            colIndex: boxIndex % 3 * 3 + cellIndex % 3
+        }
+    }
 }
 
 // 工具集
